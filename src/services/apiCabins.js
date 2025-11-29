846b8299ad78be88e886c4fedfadae7fd1d9966f
Naw San <nawsanbmo@gmail.com>
2025-11-29 21:10:47 +0900

    add createCabin function

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,11 +10,17 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin can not be created");
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
-  const { data, error } = await supabase
-    .from("cabins")
-    .delete()
-    .eq("id", id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
     console.error(error);
     throw new Error("Cabin can not be deleted");
