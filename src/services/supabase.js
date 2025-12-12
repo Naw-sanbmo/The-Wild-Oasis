6552e9471e23af44322e8f54f7f312ae19c3a0af
Naw San <nawsanbmo@gmail.com>
2025-12-12 19:49:02 +0900

    add upload-image to supabase feature

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ehshiiltihuftbyroole.supabase.co";
+export const supabaseUrl = "https://ehshiiltihuftbyroole.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoc2hpaWx0aWh1ZnRieXJvb2xlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMjIwMDIsImV4cCI6MjA3OTc5ODAwMn0.7hlQ_YRh8K7nW2c_cXiYS0tTEjZYeXbdEeNeFUKXzLE";
 const supabase = createClient(supabaseUrl, supabaseKey);
