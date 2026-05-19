edab1edc2c9e7c6de73220fcbbd4c72531faf4b8
Naw San <nawsanbmo@gmail.com>
2026-05-20 03:32:37 +0900

    add check-out booking feature and fix a small filter-related bug

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,8 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,6 +24,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
   const navigate = useNavigate();
+  const { checkout, isCheckingout } = useCheckout();
 
   const moveBack = useMoveBack();
 
@@ -52,6 +55,17 @@ function BookingDetail() {
             Checkin
           </Button>
         )}
+
+        {status === "checked-in" && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingout}
+          >
+            Check out
+          </Button>
+        )}
+        
         <Button $variation="secondary" onClick={moveBack}>
           Back
         </Button>
