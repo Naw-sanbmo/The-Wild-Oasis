36e2022ed290f9c47965a5afecf25e100afbcddb
Naw San <nawsanbmo@gmail.com>
2026-05-22 14:53:57 +0900

    add user authorization protected route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +34,13 @@ function App() {
         future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
       >
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const { isPending, user } = useUser();
+  if (isPending)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
+  return children;
+}
+
+export default ProtectedRoute;
