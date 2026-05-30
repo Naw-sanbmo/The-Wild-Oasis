60fe212ff4ca5e6e4dd5282ad90ca7a3899d05f0
Naw San <nawsanbmo@gmail.com>
2026-05-31 04:10:26 +0900

    add dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>statistic</div>
+      <div>Today activities</div>
+      <div>chart of duration</div>
+      <div>Chart of sale</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
